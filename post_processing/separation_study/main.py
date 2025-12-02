d053b8e50d6526791f3ca2e3e3cb8148294c1b7d
Hyunsoo Lee <hyunsol@g.clemson.edu>
2025-12-02 15:41:56 -0500

    update

--- post_processing/separation_study/main.py
+++ post_processing/separation_study/main.py
@@ -16,6 +16,7 @@ import matplotlib.cm as cm
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from pathlib import Path
+from collections import defaultdict
 from matplotlib import rcParams
 from mpl_toolkits.mplot3d import Axes3D  # noqa: F401
 
@@ -44,148 +45,83 @@ import pyMoDELib
 
 
 def main():
+    box_axis_idx = {"x": 0, "y": 1, "z": 2}
+
     with open("config.json", "r") as f:
         config = json.load(f)
     simulationDir = Path(config["data_path"])
     evl_step = config["evl_step"]
+    glide_axis = box_axis_idx[config["glide_axis"]]
+    line_tangent_axis = box_axis_idx[config["line_tangent_axis"]]
+    num_samples = config["number_of_samples"]
 
-    # ------------- Setup MoDELib objects -------------
-    src = simulationDir
-    tmp = None # will hold TemporaryDirectory handle
-    work_dir = None # will point to the folder we actually use
-    if src.is_dir(): # in uncompressed format
-        work_dir = src
-    elif src.suffixes == ['.tar', '.gz']: # if .tar.gz format
-        tmp = tempfile.TemporaryDirectory()
-        with tarfile.open(src, 'r:gz') as tf:
-            tf.extractall(tmp.name)
-        work_dir = pathlib.Path(tmp.name) / src.name.removesuffix('.tar.gz')
-    else:
-        raise FileNotFoundError('neither directory nor .tar.gz found')
 
     # test 100 pair
-    num_pairs = 100
-    noise_seed_to_test = range(1, num_pairs+1)
+    noise_seed_to_test = range(1, num_samples+1)
     glide_inc_step = 4
     ref_glide_pos = 400
     glide_steps_to_test = [
-        [ref_glide_pos, ref_glide_pos + (glide_inc_step * i)] for i in range(1, num_pairs+1)
+        [ref_glide_pos, ref_glide_pos + (glide_inc_step * i)] for i in range(1, num_samples+1)
     ]
 
-    ddBase = pyMoDELib.DislocationDynamicsBase(str(work_dir))
-    ddio = pyMoDELib.DDconfigIO(str(work_dir / "evl"))
 
-    # Box geometry
-    xMin = np.array(ddBase.mesh.xMin(), dtype=float)
-    xMax = np.array(ddBase.mesh.xMax(), dtype=float)
-    xCenter = np.array(ddBase.mesh.xCenter(), dtype=float)
-    # read evl file
-    ddio.readTxt(evl_step)
-    # ------------- First pass: find active planes over ALL EVLs -------------
-    active_plane_keys_global = set()
-    defectiveCrystal = pyMoDELib.DefectiveCrystal(ddBase)
-    defectiveCrystal.initializeConfiguration(ddio)
-    DN = defectiveCrystal.dislocationNetwork()
-    glidePlanes = ddBase.glidePlanes()
+    separation_dists_total = defaultdict(list)
+    for seed, glide_steps in zip(noise_seed_to_test, glide_steps_to_test):
+        work_dir = simulationDir / f"seed{seed}_steps{''.join(map(str, glide_steps))}"
+        ddBase = pyMoDELib.DislocationDynamicsBase(str(work_dir))
+        ddio = pyMoDELib.DDconfigIO(str(work_dir / "evl"))
 
-    planes = getattr(ddBase, "glidePlanes", lambda: [])()
-    plane_list = list(planes) if planes else []
-    plane_keys = [stable_plane_key(gp) for gp in plane_list]  # Not used
-    print(f" ... Total glide planes: {len(plane_keys)} ...")
+        # Box geometry
+        xMin = np.array(ddBase.mesh.xMin(), dtype=float)
+        xMax = np.array(ddBase.mesh.xMax(), dtype=float)
+        xCenter = np.array(ddBase.mesh.xCenter(), dtype=float)
 
-    node_pos = {}
-    for key in DN.networkNodes().keys():
-        node = DN.networkNodes().getRef(key)
-        node_pos[node.networkID()] = np.array(node.position(), float)
+        # read evl file
+        ddio.readTxt(evl_step)
 
-    plane_segments_by_key = {}
-    for gp in plane_list:
-        key = stable_plane_key(gp)
-        segs = gp.meshSegments() if hasattr(gp, "meshSegments") else []
-        plane_segments_by_key[key] = [
-            (np.array(P0, float), np.array(P1, float)) for (P0, P1) in segs
-        ]
+        # ------------- First pass: find active planes over ALL EVLs -------------
+        active_plane_keys_global = set()
+        defectiveCrystal = pyMoDELib.DefectiveCrystal(ddBase)
+        defectiveCrystal.initializeConfiguration(ddio)
+        DN = defectiveCrystal.dislocationNetwork()
+        planes = ddBase.glidePlanes()
 
-    plane_geometry = build_plane_geometry_from_segments(plane_segments_by_key)
-    plane_to_segments, _ = build_plane_to_segments_geometric(
-        DN, node_pos, plane_geometry, tol=1e-4
-    )
+        # find the number of glide planes
+        plane_keys = [stable_plane_key(gp) for gp in planes]  # Not used
+        print(f" ... Total glide planes: {len(plane_keys)} ...")
 
-    for pk, segs in plane_to_segments.items():
-        if len(segs) > 0:
-            active_plane_keys_global.add(pk)
+        node_pos_per_loop = defaultdict(list)
+        # collect network IDs for each loop number
+        for key in DN.networkNodes().keys():
+            node = DN.networkNodes().getRef(key)
+            node_pos = np.array(node.position(), dtype=np.float64)
+            node_pos_per_loop[tuple(node.loopIDs())].append(node_pos)
+        # filter the loopIDs that has more than 1 element (connecting nodes)
+        node_pos_per_loop = {k[0]: np.array(v) for k, v in node_pos_per_loop.items() if v and len(k)==1}
 
-    active_plane_keys_global = sorted(active_plane_keys_global)
-    print("Planes that EVER have segments:", active_plane_keys_global)
-    if not active_plane_keys_global:
-        raise RuntimeError("No active planes with segments found over the EVL range.")
+        # find loop pair
+        glide_plane_loops = defaultdict(list)
+        for loop_id, v in node_pos_per_loop.items():
+            unique_z_pos = np.unique(v[:,2]).item()
+            glide_plane_loops[unique_z_pos].append(loop_id)
+        partial_pairs = list(glide_plane_loops.values())
+        print(partial_pairs)
 
-    # Build static 2D frames (basis + edges + global x/y)
-    plane_frames = {}  # pk -> (origin, u, v, n)
-    plane_proj_edges = {}  # pk -> list of ((x0,y0), (x1,y1))
-    all_xy = []
-    for pk in active_plane_keys_global:
-        segs = plane_segments_by_key.get(pk, [])
-        if len(segs) < 1:
-            continue
-        pts = []
-        for P0, P1 in segs:
-            pts += [P0, P1]
-        if len(pts) < 3:
-            continue
-        origin, u, v, n = plane_basis_from_points(np.asarray(pts, float))
-        plane_frames[pk] = (origin, u, v, n)
+        # calc separation distance
+        #separation_dists = {}
+        for partial_pair in partial_pairs:
+            avg_line_pos = []
+            for partial_loop_id in partial_pair:
+                node_pos = node_pos_per_loop[partial_loop_id]
+                mean_line_pos = np.mean(node_pos[:, glide_axis])
+                avg_line_pos.append(mean_line_pos)
+            #separation_dists[tuple(partial_pair)] = np.abs(np.diff(avg_line_pos)) 
+            separation_dists_total[tuple(partial_pair)].append(np.abs(np.diff(avg_line_pos)))
 
-        edges2d = []
-        for P0, P1 in segs:
-            x0, y0 = project(P0, origin, u, v)
-            x1, y1 = project(P1, origin, u, v)
-            edges2d.append(((x0, y0), (x1, y1)))
-            all_xy += [(x0, y0), (x1, y1)]
-        plane_proj_edges[pk] = edges2d
+    separation_dists_total = {k: np.array(v) for k, v in separation_dists_total.items()}
 
-    if not all_xy:
-        raise RuntimeError("No projected edge points; cannot build 2D frames.")
-    all_xy = np.asarray(all_xy, float)
-
-    node_pos = {}
-    for key in DN.networkNodes().keys():
-        node = DN.networkNodes().getRef(key)
-        node_pos[node.networkID()] = np.array(node.position(), float)
-
-    plane_segments_by_key = {}
-    for gp in plane_list:
-        key = stable_plane_key(gp)
-        segs = gp.meshSegments() if hasattr(gp, "meshSegments") else []
-        plane_segments_by_key[key] = [
-            (np.array(P0, float), np.array(P1, float)) for (P0, P1) in segs
-        ]
-
-    plane_geometry = build_plane_geometry_from_segments(plane_segments_by_key)
-    plane_to_segments, _ = build_plane_to_segments_geometric(
-        DN, node_pos, plane_geometry, tol=1e-4
-    )
-
-    # convert value to nparray and remove empty list
-    plane_to_segments = {k: np.array(v) for k,v in plane_to_segments.items() if v}
-
-    # convert source_pos and sink_pos pair to plain node pos info
-    for k, v in plane_to_segments.items():
-        # stack source and sink positions (N, 2, 3) -> (N*2, 3)
-        tmp_val = v.reshape(-1, 3)
-        # Remove exact duplicate rows (source sink overlap)
-        tmp_val = np.unique(tmp_val, axis=0)
-        # replace the dictionary entry
-        plane_to_segments[k] = tmp_val
-
-    # for debugging
-    # visualizes the node positional information
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    # Scatter plot
-    for k, v in plane_to_segments.items():
-        ax.scatter(v[:,0], v[:,1], v[:,2])
-    plt.show()
+    print(separation_dists_total)
+    exit()
 
     # automatically deletes the tmp tree if there is any
     if tmp:
